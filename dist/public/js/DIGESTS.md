9ce41c86234e6fe10d49b1d8f6c7a4d04f8f7b03
Sergio Antozzi <santozzi@gmail.com>
2024-05-23 00:17:19 -0300

    update documentation

---
+++ dist/public/js/DIGESTS.md
@@ -0,0 +1,41 @@
+## Subresource Integrity
+
+If you are loading Highlight.js via CDN you may wish to use [Subresource Integrity](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity) to guarantee that you are using a legimitate build of the library.
+
+To do this you simply need to add the `integrity` attribute for each JavaScript file you download via CDN. These digests are used by the browser to confirm the files downloaded have not been modified.
+
+```html
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"
+  integrity="sha384-9mu2JKpUImscOMmwjm1y6MA2YsW3amSoFNYwKeUHxaXYKQ1naywWmamEGMdviEen"></script>
+<!-- including any other grammars you might need to load -->
+<script
+  src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"
+  integrity="sha384-WmGkHEmwSI19EhTfO1nrSk3RziUQKRWg3vO0Ur3VYZjWvJRdRnX4/scQg+S2w1fI"></script>
+```
+
+The full list of digests for every file can be found below.
+
+### Digests
+
+```
+sha384-WFJdA9Hz+G9NQx5vPba/tcGyIibm57UkKVY32wNB/94iT2FmPma5W7gY8p2l6qps /es/languages/java.js
+sha384-coaxfgI2lKuDqSxfMlfyPq5WM0THaLGyATZHzaFMrWdIbPcLdduuItTe6AmT/m33 /es/languages/java.min.js
+sha384-WCznKe2n87QvV/L1MlXN+S8R6NPUQGU34+AqogMuWGZJswSD6rt3Mgih+xuKlDgm /es/languages/javascript.js
+sha384-eGsBtetyKPDKaLiTnxTzhSzTFM6A/yjHBQIj4rAMVaLPKW5tJb8U6XLr/AikCPd+ /es/languages/javascript.min.js
+sha384-12GbYFzdyZCSmfBTmO2CR/qE89K5uE1PEuJ3QUwXH0Q9u+uoLNigjH9dG7LAxxiI /es/languages/json.js
+sha384-+DT7AtubDhVDciRc6CgjJJRsCt0L8NC3Dh8n9Tj2tZWU8rWxDIj1ViubmUDn8OCY /es/languages/json.min.js
+sha384-CS3qiWid6Sod3yAiQwgPzy2ZerR00u/cwhnMxQrETuI74o006r1p5qj1U9Gdo4uD /es/languages/typescript.js
+sha384-HHgazax8ozQ9RDWlJQyaFzaTk/OgTFRVHH+lcaYInkE8wVu5fnpkqkB3KUdzKcvE /es/languages/typescript.min.js
+sha384-pYIeBYeCE96U9EkPcT4uJjNWyrB1BKB41JIadYJbvmGa5KacaoXtSQOUpBfeyWQX /languages/java.js
+sha384-uUg+ux8epe42611RSvEkMX2gvEkMdw+l6xG5Z/aQriABp38RLyF9MjDZtlTlMuQY /languages/java.min.js
+sha384-vJxw3XlwaqOQr8IlRPVIBO6DMML5W978fR21/GRI5PAF7yYi2WstLYNG1lXk6j9u /languages/javascript.js
+sha384-44q2s9jxk8W5N9gAB0yn7UYLi9E2oVw8eHyaTZLkDS3WuZM/AttkAiVj6JoZuGS4 /languages/javascript.min.js
+sha384-dq9sY7BcOdU/6YaN+YmFuWFG8MY2WYJG2w3RlDRfaVvjdHchE07Ss7ILfcZ56nUM /languages/json.js
+sha384-RbRhXcXx5VHUdUaC5R0oV+XBXA5GhkaVCUzK8xN19K3FmtWSHyGVgulK92XnhBsI /languages/json.min.js
+sha384-yZXtQC/OmWoPykosK7vE1nCvV4E/six6+apjNau4JwBkejkea5nP7VBEJJkGnvoF /languages/typescript.js
+sha384-ORwtVEfrCZ0gzGacgmfv1wOtxcPIaVfHKwq8dKQjObRwx3qpKjsSg1ldTu1PEgXd /languages/typescript.min.js
+sha384-VTJjkVda6tfxtUbid+jPl76mG7iQeTsc82O1oh9uXzl7FKQ36EeqYJYKUU1dRpUM /highlight.js
+sha384-cNrK78VGT8GC1j8+a6Kp+BG36Rv3dkhOW446y1ff1gxMPBJuxSrW0si9dh0PS9Nj /highlight.min.js
+```
+
--- dist/tests/user.test.js
+++ dist/tests/user.test.js
@@ -15,37 +15,42 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongo_db_1 = require("../db/mongo.db");
 const app_1 = __importDefault(require("../app"));
 const supertest_1 = __importDefault(require("supertest"));
-const mongodb_1 = require("mongodb");
-const route = '/users/';
+const route = '/api/users/';
 const usuario = {
-    apellido: "Alvarez",
-    nombre: "Julian",
+    nombre: "Sergio",
+    apellido: "Antozzi",
+    email: "santozzi@gmailona.com",
     carrera: "ingenieria",
-    edad: 24,
-    email: "julian@campeones2231099.com",
-    password: "123454aA"
+    edad: 40,
+    password: "12345Aa",
+    images: [
+        "https://e7.pngegg.com/pngimages/340/946/png-clipart-avatar-user-computer-icons-software-developer-avatar-child-face-thumbnail.png"
+    ]
 };
 beforeAll(() => __awaiter(void 0, void 0, void 0, function* () { return yield (0, mongo_db_1.mongooseConnection)(); }));
-describe('GET /users', () => {
-    test('should respond with a 200 status code', () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield (0, supertest_1.default)(app_1.default).get(route).send();
+let token;
+describe('POST /users', () => {
+    test('login', () => __awaiter(void 0, void 0, void 0, function* () {
+        let response = yield (0, supertest_1.default)(app_1.default).post(route + "login").send({ email: "santozzi@gmail.com", password: "12345Aa" });
+        const token = (yield JSON.parse(response.text)).token;
+        console.log(token);
         expect(response.statusCode).toBe(200);
-    }));
-    test('should respond with an array', () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield (0, supertest_1.default)(app_1.default).get(route).send();
-        expect(response.body).toBeInstanceOf(Array);
+        let id = "";
+        response = yield (0, supertest_1.default)(app_1.default).post(route + "create").send(usuario);
+        const nuevoUsuario = JSON.parse(response.text);
+        if (nuevoUsuario._id)
+            id = nuevoUsuario._id;
+        expect(response.statusCode).toBe(200);
+        const authHeaders = { Authorization: token };
+        response = yield (0, supertest_1.default)(app_1.default).delete(route + "delete/" + id).set(authHeaders).send();
+        expect(response.statusCode).toBe(200);
     }));
 });
-describe('POST /users', () => {
-    let id;
+describe('GET /users', () => {
+    const authHeaders = { Authorization: token };
     test('should respond with a 200 status code', () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield (0, supertest_1.default)(app_1.default).post(route).send(usuario);
-        const nuevoUsuario = JSON.parse(response.text);
-        id = new mongodb_1.ObjectId(nuevoUsuario._id);
+        const response = yield (0, supertest_1.default)(app_1.default).get(route).set(authHeaders).send();
         expect(response.statusCode).toBe(200);
-    }));
-    test('should response ok', () => __awaiter(void 0, void 0, void 0, function* () {
-        console.log(yield (0, supertest_1.default)(app_1.default).delete(route + id));
     }));
 });
 afterAll(() => __awaiter(void 0, void 0, void 0, function* () { return yield (0, mongo_db_1.mongooseDisconect)(); }));
