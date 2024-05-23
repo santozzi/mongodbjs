49981a1d49b0235a455b26502bb208c395ff4392
Sergio Antozzi <santozzi@gmail.com>
2024-05-23 07:21:42 -0300

    routes product

--- dist/routes/product.routes.js
+++ dist/routes/product.routes.js
@@ -6,9 +6,9 @@ const product_controller_1 = require("../controllers/product.controller");
 const productRoute = (0, express_1.Router)();
 //endpoints
 productRoute.post("/create", verifyToken_middleware_1.verifyTokenMiddleware, product_controller_1.create);
-productRoute.get("/", verifyToken_middleware_1.verifyTokenMiddleware, product_controller_1.findAll);
+productRoute.get("/",  product_controller_1.findAll);
 //por params le paso name
-productRoute.get("/:id", verifyToken_middleware_1.verifyTokenMiddleware, product_controller_1.findById);
+productRoute.get("/:id",  product_controller_1.findById);
 productRoute.delete("/delete/:id", verifyToken_middleware_1.verifyTokenMiddleware, product_controller_1.deleteProduct);
 productRoute.put("/update/:id", verifyToken_middleware_1.verifyTokenMiddleware, product_controller_1.update);
 exports.default = productRoute;
